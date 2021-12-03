4534c1d53e047be06f96aebc8800238be51e7553
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-12-02 19:31:03 -0600

    updating sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,26 @@
 import { NavLink } from "react-router-dom"
 import SidebarData from './SidebarData'
 import "../css/dashboard.css"
+
+const Sidebar = () => {
+    return (
+        <>
+            <div className="sidebar">
+                <div className="d-flex flex-column flex-shrink-0 bg-light">
+                    <span className="d-block p-3 link-dark text-decoration-none" data-bs-toggle="tooltip" data-bs-placement="right">
+                        <h6 className="fw-bolder text-center" style={{textShadow:"1px 1px 10px lightblue"}}>Studdy Buddy</h6>
+                    </span>
+                    <hr className="m-1" />
+                    <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
+                        {
+                            
+                        }
+                    </ul>
+                </div>
+
+            </div>
+
+            
+        </>
+    )
+}
