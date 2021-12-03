1d7020d077c053158e461a44432882492302a62e
RonaldoS542274 <77812398+RonaldoS542274@users.noreply.github.com>
2021-12-02 19:28:27 -0600

    updating some changes in code

--- src/components/DataFetching.jsx
+++ src/components/DataFetching.jsx
@@ -15,3 +15,32 @@ const DataFetching = () => {
         callBack();
     }, []);
 
+    return (
+        <>
+            {
+                !Data.length ?
+                    <h1 className="text-secondary " style={{margin:"200px",height:"100vh",textShadow: "none", fontSize: "2rem" }}>Please upload course schedule file sheet <Link to="/course">Click here</Link></h1> :
+                    <ul className="fetch-data">
+                        {Data.map((data, index) => {
+                            return (
+                                <li key={index}>
+                                    <div className="card" style={{ width: "18rem" }}>
+                                        <img className="card-img-top" src={data.img} alt="Card img cap" />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{data.subject}</h5>
+                                            <p className="card-text">{data.prof}</p>
+                                            <a href="/dashboard" className="btn btn-primary">About</a>
+                                        </div>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+            }
+            {/* <h1>hii</h1> */}
+        </>
+
+    )
+}
+
+export default DataFetching
