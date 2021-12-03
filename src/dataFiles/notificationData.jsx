919a2937290ef005f2a07b2069fb6d631f769043
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-12-02 19:26:04 -0600

    updating courses

--- src/dataFiles/notificationData.jsx
+++ src/dataFiles/notificationData.jsx
@@ -1,5 +1,11 @@
 export default Notification = [{
     from: "Java",
     message: "Your syllabus is complete"
-}, 
+},{
+    from: "DBMS",
+    message: "Create one mini project with include mysql database"
+}, {
+    from: "Data science",
+    message: "Your syllabus is complete"
+} 
 }]
