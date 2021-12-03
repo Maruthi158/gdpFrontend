bc14bdf33e663ff0b03ad40cd6be986c75ef2ca3
GUNDAANUTEJ <77635770+GUNDAANUTEJ@users.noreply.github.com>
2021-12-02 19:54:57 -0600

    updating code in notification.jsx

--- src/components/notification.jsx
+++ src/components/notification.jsx
@@ -1,1 +1,15 @@
-            
+import '../css/dashboard.css'
+import Sidebar from "./Sidebar"
+import notificationData from "../dataFiles/notificationData"
+
+const Notification = () => {
+
+    const history = useHistory()
+    useEffect(() => {
+        const callBack = async () => {
+            const result = await fetch("/dashboard", {
+                method: "GET",
+                headers: {
+                    "content-type": "application/json"
+                }
+            })       
