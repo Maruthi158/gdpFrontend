c43c7b7f11644150a9103fc4060d556a103a8ca0
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-12-02 19:26:34 -0600

    updating courses

--- src/dataFiles/notificationData.jsx
+++ src/dataFiles/notificationData.jsx
@@ -7,5 +7,10 @@ export default Notification = [{
 }, {
     from: "Data science",
     message: "Your syllabus is complete"
-} 
+}, {
+    from: "Computer Organization",
+    message: "Assignment-10 has been submission by monday"
+}, {
+    from: "Web apps",
+    message: "Mid Exams  will be taken on wednesday"
 }]
