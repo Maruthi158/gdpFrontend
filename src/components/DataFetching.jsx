8b430e80a7ef20c5f594b49a643186200721f4d4
RonaldoS542274 <77812398+RonaldoS542274@users.noreply.github.com>
2021-12-02 19:27:22 -0600

    made changes

--- src/components/DataFetching.jsx
+++ src/components/DataFetching.jsx
@@ -2,3 +2,16 @@ import axios from 'axios'
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
+const DataFetching = () => {
+    const [Data, setData] = useState([])
+
+    useEffect(() => {
+        const callBack = () => {
+            axios.get("/fetchData")
+                .then((result) => {
+                    setData((result.data))
+                })
+        }
+        callBack();
+    }, []);
+
