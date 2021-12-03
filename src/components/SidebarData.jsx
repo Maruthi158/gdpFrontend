f24819a2b6c85c55500be6b2f36967f2922a686a
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-12-02 19:33:23 -0600

    Update SidebarData.jsx

--- src/components/SidebarData.jsx
+++ src/components/SidebarData.jsx
@@ -3,4 +3,16 @@ const SidebarData = [
         img: "https://cdn-icons-png.flaticon.com/128/1828/1828791.png",
         title: "Dashboard",
         link: '/dashboard'
-    }]
+    }
+,
+    {
+        img: "https://cdn-icons-png.flaticon.com/128/2000/2000920.png",
+        title: "Courses",
+        link: '/course'
+    },
+    {
+        img: "https://cdn-icons-png.flaticon.com/512/1156/1156949.png",
+        title: "Notification",
+        link: '/notification'
+    } 
+]
