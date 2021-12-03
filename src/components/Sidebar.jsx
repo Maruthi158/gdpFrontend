84f3313149d01ea84dc5a157bf844f4c9decf46b
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-12-02 19:31:33 -0600

    updating sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,7 +13,16 @@ const Sidebar = () => {
                     <hr className="m-1" />
                     <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
                         {
-                            
+                            SidebarData.map((data, index) => {
+                                return (
+                                    <li className="nav-item" key={index}>
+                                        <NavLink exact to={data.link} activeClassName="active text-white" className="nav-link py-3 border-bottom text-black" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right">
+                                            <center><img src={data.img} className="img-fluid  pb-0" alt="source img" height="30" width="30" /><font style={{fontSize:"13px"}}>{data.title}</font></center>
+                                        </NavLink>
+                                        <hr className="m-1"/>
+                                    </li>
+                                )
+                            })
                         }
                     </ul>
                 </div>
@@ -24,3 +33,5 @@ const Sidebar = () => {
         </>
     )
 }
+
+export default Sidebar
