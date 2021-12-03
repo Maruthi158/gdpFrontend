aa3af5ec0982927b93cf77a4977465391dff7fed
GUNDAANUTEJ <77635770+GUNDAANUTEJ@users.noreply.github.com>
2021-12-02 19:30:26 -0600

    added file with course

---
+++ src/components/Course.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "./Sidebar"
+import path from 'path'
+//import { Console } from "console"
+// import axios from 'axios'
+
+const Course = () => {
+
+    const history = useHistory()
+    useEffect(() => {
+        const callBack = async () => {
